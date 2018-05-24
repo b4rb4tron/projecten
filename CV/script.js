5cbbf0f88d87ccd8a25bc5b103d35c9a3907d42d
Roger van Hout <roger@snoeivanhout.nl>
2018-05-24 09:19:29 +0200

    cv

--- CV/script.js
+++ CV/script.js
@@ -1,55 +1,57 @@
 //
-var wrapper = document.getElementById("wrapper");
+var $ = id => document.getElementById(id);
 var htmlX = 0;
 var cssX = 0;
 var jsX = 0;
-var wrapperY = 0;
-var upDown = wrapper.getBoundingClientRect().y;
-var scrollBinnen = document.getElementById("wrapper");
+var scrollDirection = $("wrapper").getBoundingClientRect().y;
+var up;
+
 document.addEventListener("scroll", scrollDingen);
 
 function scrollDingen() {
-  if (wrapper.getBoundingClientRect().y < upDown) {
-    upDown = wrapper.getBoundingClientRect().y;
-    jsX += 4;
-    htmlX += 3;
-    cssX += 6;
+  //check scrolling up or down
+  if ($("wrapper").getBoundingClientRect().y < scrollDirection) {
+    scrollDirection = $("wrapper").getBoundingClientRect().y;
+    up = false;
   } else {
-    jsX -= 4;
+    up = true;
+    scrollDirection = $("wrapper").getBoundingClientRect().y;
+  }
+  //html balk
+  if (
+    $("wrapper").getBoundingClientRect().y < 0 &&
+    $("groen").getBoundingClientRect().width < 70 &&
+    up == false
+  ) {
+    htmlX += 3;
+    $("groen").style.width = htmlX + "px";
+  } else if (up == true && $("groen").getBoundingClientRect().width > 0) {
     htmlX -= 3;
-    cssX -= 6;
-    upDown = wrapper.getBoundingClientRect().y;
+    $("groen").style.width = htmlX + "px";
   }
+  //css balk
   if (
-    wrapper.getBoundingClientRect().y < 0 &&
-    document.getElementById("groen").getBoundingClientRect().width < 170
+    $("wrapper").getBoundingClientRect().y < 0 &&
+    $("rood").getBoundingClientRect().width < 250 &&
+    up == false
   ) {
-    document.getElementById("groen").style.width = htmlX + "px";
+    cssX += 4;
+    $("rood").style.width = cssX + "px";
+  } else if ($("rood").getBoundingClientRect().width > 0 && up == true) {
+    cssX -= 4;
+    $("rood").style.width = cssX + "px";
   }
-
+  //javascript balk
   if (
-    wrapper.getBoundingClientRect().y < 0 &&
-    document.getElementById("rood").getBoundingClientRect().width < 250
+    $("wrapper").getBoundingClientRect().y < 0 &&
+    $("blauw").getBoundingClientRect().width < 200 &&
+    up == false
   ) {
-    document.getElementById("rood").style.width = cssX + "px";
+    jsX += 5;
+    $("blauw").style.width = jsX + "px";
+  } else if ($("blauw").getBoundingClientRect().width > 0 && up == true) {
+    jsX -= 5;
+    $("blauw").style.width = jsX + "px";
   }
-
-  if (
-    wrapper.getBoundingClientRect().y < 0 &&
-    document.getElementById("blauw").getBoundingClientRect().width < 200
-  ) {
-    document.getElementById("blauw").style.width = jsX + "px";
-  }
-
-  if( wrapper.getBoundingClientRect().y < -1000){
-  
-      document.getElementById("left30").style.top = -(wrapperY) + "px";
-      document.getElementById("left30").style.position = "relative";
-      }else if (wrapper.getBoundingClientRect().y > wrapperY){
-        
-        document.getElementById("left30").style.top = -(wrapperY) + "px";
-      } else {
-        console.log(wrapperY);
-          wrapperY = wrapper.getBoundingClientRect().y;
-  }
+  console.log($("left30").getClientRects()) ;
 }
