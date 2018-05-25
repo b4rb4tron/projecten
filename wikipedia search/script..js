cd4b1e33d61e8a8f8e8da705f280ac29e18f9b10
b4rb4tron <roger@snoeivanhout.nl>
2018-05-25 13:19:26 +0200

    wikipedia viewer finished

--- wikipedia search/script..js
+++ wikipedia search/script..js
@@ -1,8 +1,35 @@
-let klik = 0;
-//submit = () => document.getElementById("hoi").innerHTML = document.getElementById("searchField").value;
-function submit(e) { if (e.keyCode == 13){
-    document.getElementById("hoi").innerHTML = document.getElementById("searchField").value;
+let r = document.getElementById("results");
+function submit(e) {
+  if (e.keyCode == 13) {
+      r.innerHTML= "";
+      setTimeout(function() {document.getElementById("searchField").value = "";}, 1000)
+;    $.ajax({
+      type: "GET",
+      url: "https://en.wikipedia.org/w/api.php",
+      data: {
+        action: "query",
+        format: "json",
+        prop: "extracts",
+        exchars: "200",
+        exlimit: "max",
+        explaintext: "",
+        exintro: "",
+        pilimit: "max",
+        rawcontinue: "",
+        generator: "search",
+        gsrsearch: document.getElementById("searchField").value,
+        gsrnamespace: "0",
+        gsrlimit: "10"
+      },
+      dataType: "JSONP",
+      success: function(response) {
+        for (var pageid in response.query.pages) {
+            r.innerHTML += "<div class='resultaat' style='margin-top: 5px; transition: 1s;'><a href='http://en.wikipedia.org/?curid=" + [pageid]  + "' target='_blank'> <h4>" + response.query.pages[pageid].title + "</h4><p>" + response.query.pages[pageid].extract + "</p></a></div>";
+        }
+      }
+    });
+  }
 }
-}
-document.getElementById("searchField").addEventListener("keyup", submit );
 
+
+document.getElementById("searchField").addEventListener("keyup", submit);
