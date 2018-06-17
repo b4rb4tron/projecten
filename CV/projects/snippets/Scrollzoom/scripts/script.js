85a6027e0c6fc2cd8b0b8a96e33c942f4ccc654c
b4rb4tron <roger@snoeivanhout.nl>
2018-06-17 17:31:56 +0200

    cv updated

---
+++ CV/projects/snippets/Scrollzoom/scripts/script.js
@@ -0,0 +1,135 @@
+let text = `dolor sit amet consectetur adipisicing elit. Quasi, sed fuga earum consequuntur numqua
+temporibus laborum mollitia voluptatibus aut cum impedit officiis, ducimus dolores veniam. Cumque voluptates,
+magni repudiandae quia consequatur sit maxime nulla illum ad tempore. Ipsam cupiditate doloribus similique
+veritatis nisi tenetur non tempora magnam, qui dignissimos eveniet commodi odit, molestias laborum, maiores
+modi! Odit laudantium, laborum at saepe nihil eum consequatur mollitia quis nisi dignissimos dolore excepturi
+quas in incidunt ad animi quos aliquam iste adipisci error officia reprehenderit quibusdam? Autem voluptas
+atque laudantium dolor explicabo pariatur dignissimos, ab cum eveniet eum ipsum? Praesentium, alias? Eveniet
+facilis ad accusantium, obcaecati perspiciatis laborum, nesciunt similique nobis nihil magni sint esse ratione
+voluptas inventore asperiores nemo officia! Illum a impedit eveniet voluptatibus eos veritatis eum, libero
+nam magni quos, nostrum pariatur amet itaque facere exercitationem. Debitis voluptate nemo dolores minima
+sit vero? Eaque possimus, officia obcaecati voluptatem ea architecto nemo numquam quisquam corporis incidunt
+unde hic veniam itaque quasi amet perspiciatis alias deserunt, laborum ab libero, doloribus illo! Exercitationem
+impedit culpa adipisci dicta tempore. Veniam modi porro error assumenda ullam pariatur odio, blanditiis ab
+eveniet necessitatibus temporibus tempore quaerat cum, reiciendis labore, ratione maxime. Eligendi necessitatibus
+doloremque reiciendis esse. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto cum sed
+facere maiores distinctio in iusto sit nihil assumenda qui blanditiis, pariatur non praesentium dolores delectus
+magnam quas, tempora ipsam ad molestiae facilis repudiandae, consequuntur itaque? Beatae quo aliquam vel,
+necessitatibus voluptate aperiam reiciendis molestiae ducimus officiis eveniet dignissimos sit rerum voluptas
+autem iste earum nesciunt eum dolor odio ad officia. Voluptas, esse labore libero tempore ipsa ad quibusdam!
+Quia, quaerat tenetur itaque quis id iste cum deserunt pariatur porro voluptates debitis a dolorem sapiente
+cupiditate eligendi! Vitae animi quasi porro placeat dicta nostrum incidunt facilis nisi praesentium, deserunt
+voluptatum rerum quibusdam mollitia laborum cupiditate quam saepe repudiandae! Natus corrupti animi similique
+doloribus nam. Eveniet saepe officiis obcaecati ullam fuga. Suscipit aperiam totam ad alias vel illo neque
+fugit accusamus illum porro non ipsa, laborum sequi aspernatur omnis perferendis unde voluptatem nam atque
+quis molestiae quia sit doloribus fuga. Provident molestiae consequuntur explicabo qui eaque. Error accusantium
+optio officia praesentium totam necessitatibus molestias quasi dolore maiores. Et reiciendis excepturi repellendus
+aliquam? Praesentium officiis error dignissimos labore. Eligendi et, delectus dolore ducimus atque libero
+beatae enim voluptate nostrum, quam consequuntur expedita corporis eveniet quidem minima ratione nemo, repudiandae
+fugiat accusantium suscipit. Maiores vitae alias sapiente aspernatur ex, laboriosam voluptatibus! Est labore,
+harum iusto ratione expedita illum voluptatibus atque pariatur quaerat inventore dolorum maiores ipsa illo
+similique minus odit dolore consequatur eaque nostrum qui delectus nihil! A quod, at rerum ab ducimus nisi
+unde! Praesentium quia itaque laborum cumque nemo excepturi. Fuga voluptate, accusamus quo eius reprehenderit
+illum est repudiandae perspiciatis quod aut dicta maiores veritatis nostrum! Ex rerum tempore architecto
+sed recusandae inventore magnam amet harum repellendus fugit at, iste praesentium temporibus consectetur
+sequi non in vero, aliquid eum illo quia pariatur laudantium? Exercitationem perferendis itaque illum, ea
+nesciunt pariatur facere dolorum molestias fuga est saepe, at incidunt similique labore earum qui blanditiis
+laborum, optio tenetur voluptatibus corporis quia veniam. Harum voluptate nisi dolore, quasi inventore totam
+reiciendis tenetur id aut eveniet eligendi cum beatae expedita pariatur. Neque voluptates libero in dicta
+nam voluptatum facilis animi mollitia atque vel illo itaque tempora fuga quis repudiandae, fugiat dolorum
+perferendis accusamus numquam nemo? Expedita dolorum libero sequi? Dolorem, maxime fuga. Amet molestias quibusdam
+aut enim incidunt debitis soluta ea facilis aliquam dolor. Sapiente enim ratione sequi repellendus dicta
+assumenda iusto provident expedita, quaerat, nemo voluptate ex! Mollitia a facere consectetur! Voluptatibus
+incidunt aliquam consequuntur expedita impedit, laboriosam modi dolore adipisci ipsum odio, molestias vero,
+non culpa id sapiente ratione nam rem minus possimus veniam ad ut eligendi excepturi! Modi praesentium excepturi
+fuga tenetur sit non, quibusdam hic nisi ad autem tempora mollitia quasi atque beatae est sint. Exercitationem
+ipsa, porro hic nemo atque, quis praesentium aliquid iste mollitia, ipsum aliquam blanditiis veritatis at
+voluptates autem suscipit reiciendis sed labore quo nulla itaque officiis? Possimus facilis, hic, unde aliquid
+repellat consequuntur suscipit, quaerat ipsam dolorum voluptatem dolore? Quibusdam ut obcaecati ratione nihil
+impedit. Reprehenderit eaque excepturi error eligendi harum blanditiis atque illum modi fugit fugiat soluta
+sint, a distinctio aspernatur suscipit deleniti. Aut impedit repudiandae sed dolores, animi illum error eaque
+minus eum quisquam, sint numquam magnam blanditiis. Suscipit ab culpa perferendis omnis libero incidunt sint
+alias nisi laudantium iusto, voluptates similique ea rem. Veniam eveniet accusamus deleniti repellendus,
+temporibus molestiae consequatur nemo assumenda vel sequi voluptatem expedita recusandae tempore laudantium
+eum nostrum laborum id et quasi enim magni nobis? Eaque quod atque impedit ipsam fugiat, iure voluptatibus.
+Totam ex, laboriosam magnam natus rem quod hic nemo magni itaque provident recusandae quibusdam sapiente
+qui tempora, repellendus consequuntur sed. Accusamus maiores tenetur vitae aspernatur, pariatur sunt hic
+repellat? Voluptates, consequatur recusandae. Earum nihil culpa nostrum. Debitis facere vitae tempora suscipit
+obcaecati praesentium reprehenderit iure, doloribus eaque totam quo consequuntur fuga magni in ea veniam
+possimus quibusdam. Eveniet veniam doloribus vero quae aliquam unde nihil porro quisquam quam, magnam consequatur
+beatae alias, nostrum, minima aliquid necessitatibus animi aspernatur mollitia repellendus autem iure? Nobis
+incidunt animi blanditiis rem obcaecati. Provident fuga deleniti nisi natus similique velit totam quia? Quidem
+odio minima culpa consequatur rerum dolorem impedit, ullam assumenda non deleniti omnis eaque itaque repudiandae
+iste similique soluta facilis. Cum fuga hic eum assumenda distinctio molestias officiis accusantium velit
+rerum. Alias sed culpa possimus numquam adipisci quaerat natus quidem porro, sequi quo nam placeat quia,
+eum necessitatibus. Sequi excepturi aspernatur officia perferendis, nobis voluptate esse perspiciatis unde
+sint, quam ab molestias totam assumenda saepe omnis! Error et accusantium vel? Ut debitis mollitia voluptas
+reiciendis quidem eius nulla quod nostrum tempore aliquid in cupiditate quia quos eaque sed et illo, consequatur
+nesciunt? Veniam doloremque cum magni ducimus esse nemo saepe aliquam doloribus numquam rem laboriosam libero
+voluptatem aperiam sapiente necessitatibus minus non architecto accusantium, sed, dolor aliquid ullam alias
+reiciendis. Dolorem repudiandae, est aperiam ratione quo rerum ullam eveniet? `;
+let wrapper = document.querySelector(".wrapper");
+let tx, ty, midY;
+midY = ty / 2 - wrapper.getBoundingClientRect().y;
+tx = window.innerWidth;
+ty = window.innerHeight;
+let char = Math.floor(tx / 100 * 80 / 12);
+let lines = Math.ceil(text.length / char);
+
+for (i = 0; i < lines; i++) {
+  if (text.length > char) {
+    document.getElementById("text").innerHTML +=
+      "<div id=" + i + "text>" + text.slice(0, char) + "<div>";
+    text = text.slice(char, text.length);
+  } else {
+    document.getElementById("text").innerHTML +=
+      "<div id=" + i + "text>" + text + "<div>";
+  }
+}
+
+function positions() {
+  midY = ty / 2 - wrapper.getBoundingClientRect().y;
+  for (i = 0; i < lines; i++) {
+    let ids = document.getElementById(i + "text");
+    if (
+      ids.getBoundingClientRect().y <
+        midY + 40 + wrapper.getBoundingClientRect().y &&
+      ids.getBoundingClientRect().y >
+        midY - 40 + wrapper.getBoundingClientRect().y
+    ) {
+      //ids.style.transition = "transform .2s";
+      ids.style.transformOrigin = "20% 50%";
+      ids.style.transform = "scale(1.1, 1.1)";
+      ids.style.lineHeight = "1.1em";
+      ids.style.color = "#ccc";
+      ids.className += "text"; 
+      if (
+        ids.getBoundingClientRect().y <
+          midY + 20 + wrapper.getBoundingClientRect().y &&
+        ids.getBoundingClientRect().y >
+          midY - 20 + wrapper.getBoundingClientRect().y
+      ) {
+        ids.style.transform = "scale(1.2, 1.2)";
+        ids.style.lineHeight = "1.2em";
+        ids.style.color = "#eee";
+        if (
+          ids.getBoundingClientRect().y <
+            midY + 10 + wrapper.getBoundingClientRect().y &&
+          ids.getBoundingClientRect().y >
+            midY - 10 + wrapper.getBoundingClientRect().y
+        ) {
+          ids.style.transform = "scale(1.25, 1.25)";
+          ids.style.lineHeight = "1.3em";
+          ids.style.color = "#fff";
+        }
+      }
+    } else {
+      ids.style.transform = "scale(1, 1)";
+      ids.style.lineHeight = "1em";
+      ids.style.color = "#aaa";
+    }
+  }
+}
+
+addEventListener("scroll", positions);
+window.addEventListener("load", positions);
